9cab1f0e8271b679fa5a7d26b9027f3bb8908539
Ashley Bowers <ashleymariebowers@gmail.com>
2019-04-10 09:46:45 -0400

    Changes for Albums.

--- my-angular-albums/src/app/app.component.ts
+++ my-angular-albums/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Album } from "./albums/album.model";
+import { ALBUMS } from "./albums/album.data";
 
 @Component({
   selector: 'app-root',
@@ -7,74 +8,18 @@ import { Album } from "./albums/album.model";
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  title = 'my-angular-albums';
+  title = 'My Angular Albums';
   albumsArray: Album[];
   titleCounter: number;
   numbers: number[];
 
   ngOnInit(): void {
+    this.albumsArray = ALBUMS;
     this.titleCounter = 1;
     this.numbers = [1, 2, 3];
 
-    this.albumsArray = [
-      {
-        id: 1,
-        artist: 'Tremonti',
-        album_name: 'Dust',
-        genre: 'Pop/Rock',
-        price: 11.99,
-        currency: 'USD',
-        on_sale: true,
-        year: 2016,
-        release_date: 'April 29, 2016',
-        recording_location: 'Studio Barbarosa, Orlando, FL',
-        duration: '43:18:00',
-        url: 'https://www.allmusic.com/album/dust-mw0002918360',
-      },
-      {
-        id: 2,
-        artist: 'Bon Jovi',
-        album_name: '7800 Fahrerenheit',
-        on_sale: false,
-        price: 7,
-        year: 1985,
-        currency: 'USD',
-        release_date: 'April 20, 1985',
-        recording_location: 'Warehouse, Philadelphia, PA',
-        genre: 'Pop/Rock',
-        duration: '47:15:00',
-        url:
-          'https://www.allmusic.com/album/7800%C2%B0-fahrenheit-mw0000189199',
-      },
-      {
-        id: 3,
-        artist: 'The Beatles',
-        album_name: 'The White Album',
-        on_sale: true,
-        currency: 'GBP',
-        price: 24,
-        year: 1968,
-        release_date: 'November 22, 1968',
-        recording_location: '',
-        genre: 'Pop/Rock',
-        duration: '1:33:43',
-        url:
-          'https://www.allmusic.com/album/the-beatles-white-album-mw0000418113',
-      },
-    ];
-
-    function updatePrice() {
-      this.titleCounter++;
-      this.albumsArray[0].price += 10;
-    }
-
-    const interval = setInterval(() => updatePrice, 2000);
-    this.numbers.push(4);
+    const interval = setInterval(() => { this.titleCounter++; this.albumsArray[0].price += 10; this.numbers.push(4); }, 2000);
     setTimeout(() => clearInterval(interval), 6000);
-    console.log(this.albumsArray[0].price += 10);
     this.numbers = [...this.numbers];
-
   }
-
-
 }
